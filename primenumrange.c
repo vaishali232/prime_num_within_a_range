2ef39916846361cf0c1a652b8353cf26a9a3e69c
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-08-08 12:25:58 +0530

    Create primenumrange.c

---
+++ primenumrange.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+unsigned long long int start,end,flag=0,i,j;
+scanf("%llu %llu",&start,&end);
+for(i=start+1;i<end;i++)
+{
+for(j=1;j<=i/2;j++)
+{
+if(i%j==0)
+{
+flag=1;
+break;
+
+}
+}
+if(flag==0)
+{
+printf("%llu ",i);
+
+}
+}
